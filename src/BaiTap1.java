77927c18d6a8a88a81e6e062cbfcaeacc3c86a4b
HuynhBao <huynhbao0626@gmail.com>
2025-05-26 17:14:51 +0700

    update dowhile

--- src/BaiTap1.java
+++ src/BaiTap1.java
@@ -3,39 +3,42 @@ import java.util.Scanner;
 public class BaiTap1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("1: Tính chiều dài cạnh huyền của một tam giác vuông");
-        System.out.println("2: Tính giá trị đơn thức P(x) = ax^n với 8x cho trước");
-        System.out.println("3: Tính nhập số nguyên dương n với 2 ký số");
-        System.out.println("4: Tính gi trị trung bình của 5 số nđược nhập từ bàn phím");
-        System.out.println("5: Tính độ C Sang Độ F");
-        System.out.println("6: Tính tỉ giá VND-USD");
-        System.out.println("Chọn 1 Chương Trình :");
-        int key = sc.nextInt();
-        if(key < 1){
-            System.out.println("Vui lòng nhập số bài lớn hơn 1");
-        }
-        switch (key) {
-            case 1 :
-                Bai1();
-                break;
-            case 2 :
-                Bai2();
-                break;
-            case 3 :
-                Bai3();
-                break;
-            case 4 :
-                Bai4();
-                break;
-            case 5 :
-                Bai5();
-                break;
-            case 6 :
-                Bai6();
-                break;
-            default :
-                System.out.println("Vui lòng nhập lại!!!");
-        }
+        int key;
+        do{
+            System.out.println("1: Tính chiều dài cạnh huyền của một tam giác vuông");
+            System.out.println("2: Tính giá trị đơn thức P(x) = ax^n với 8x cho trước");
+            System.out.println("3: Tính nhập số nguyên dương n với 2 ký số");
+            System.out.println("4: Tính gi trị trung bình của 5 số nđược nhập từ bàn phím");
+            System.out.println("5: Tính độ C Sang Độ F");
+            System.out.println("6: Tính tỉ giá VND-USD");
+            System.out.println("Chọn 1 Chương Trình :");
+            key = sc.nextInt();
+            if(key < 1){
+                System.out.println("Vui lòng nhập số bài lớn hơn 0");
+            }
+            switch (key) {
+                case 1 :
+                    Bai1();
+                    break;
+                case 2 :
+                    Bai2();
+                    break;
+                case 3 :
+                    Bai3();
+                    break;
+                case 4 :
+                    Bai4();
+                    break;
+                case 5 :
+                    Bai5();
+                    break;
+                case 6 :
+                    Bai6();
+                    break;
+                default :
+                    System.out.println("Vui lòng nhập lại!!!");
+            }
+        }while (key != 0);
     }
 
     public static void Bai1() {
@@ -102,7 +105,7 @@ public class BaiTap1 {
 
         double tinhTrungBinh = sum / a.length;
 
-        System.out.printf("Giá trị trung binh của 5 số là : %.2f", tinhTrungBinh);
+        System.out.printf("Giá trị trung binh của 5 số là : %.2f\n", tinhTrungBinh);
     }
 
     public static void Bai5(){
@@ -112,7 +115,7 @@ public class BaiTap1 {
 
         double doF = doC * 1.8 + 32;
 
-        System.out.printf("Độ C Sang F là : %.2fF", doF);
+        System.out.printf("Độ C Sang F là : %.2fF\n", doF);
     }
 
     public static void Bai6(){
@@ -125,6 +128,6 @@ public class BaiTap1 {
 
         double toVnd = tyGia * usd;
 
-        System.out.printf("Số tiền USD sang VND là: %.2f", toVnd);
+        System.out.printf("Số tiền USD sang VND là: %.2f\n", toVnd);
     }
 }
