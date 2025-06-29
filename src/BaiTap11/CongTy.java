15e01e1cd395e02b1d6ac1b946e5f8065bc3befe
HuynhBao <huynhbao0626@gmail.com>
2025-06-29 19:36:05 +0700

    them menu 6 den 11

--- src/BaiTap11/CongTy.java
+++ src/BaiTap11/CongTy.java
@@ -6,8 +6,10 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+
 
 @Getter
 @Setter
@@ -20,13 +22,11 @@ public class CongTy {
     private ArrayList<NhanSu> nhanSu = new ArrayList<>();
 
     public void taoDuLieuGia() {
-        // TruongPhong
         TruongPhong tp1 = new TruongPhong("TP01", "Lê Văn A", "0969026600", 12); // có nhân viên
         TruongPhong tp2 = new TruongPhong("TP02", "Lê Thị B", "0909000002", 20);     // có nhân viên
         TruongPhong tp3 = new TruongPhong("TP03", "Phan Văn C", "0909000010", 18);   // chưa có ai
 
-        // NV
-        // Phân bổ cho tp1
+
         NhanVien nv1 = new NhanVien("NV01", "Trần Văn C", "0909000003", 20);
         NhanVien nv2 = new NhanVien("NV02", "Phạm Thị D", "0909000004", 19);
         nv1.setTruongPhong(tp1);
@@ -34,7 +34,6 @@ public class CongTy {
         tp1.getNhanVien().add(nv1);
         tp1.getNhanVien().add(nv2);
 
-        // Phân bổ cho tp2
         NhanVien nv3 = new NhanVien("NV03", "Hoàng Văn E", "0909000005", 18);
         NhanVien nv4 = new NhanVien("NV04", "Đỗ Thị F", "0909000006", 22);
         nv3.setTruongPhong(tp2);
@@ -42,11 +41,9 @@ public class CongTy {
         tp2.getNhanVien().add(nv3);
         tp2.getNhanVien().add(nv4);
 
-        // NV chưa phân bổ
         NhanVien nv5 = new NhanVien("NV05", "Ngô Văn G", "0909000011", 21);
         NhanVien nv6 = new NhanVien("NV06", "Vũ Thị H", "0909000012", 19);
 
-        // Giám Đốc
         GiamDoc gd1 = new GiamDoc("GD01", "Huỳnh Chí Bảo", "0969026600", 30, 50.0);
         GiamDoc gd2 = new GiamDoc("GD02", "Mai Thị H", "0909000008", 24, 40.0);
         gd1.setCongTy(this);
@@ -98,14 +95,12 @@ public class CongTy {
 
     public void phanBoNhanVienChoTruongPhong(Scanner sc) {
 
-        //lay dsNhanVien chưa phân bổ
         List<NhanVien> dsNhanVien = getNhanSu().stream()
                 .filter(ns -> ns instanceof NhanVien)
                 .map(ns -> (NhanVien) ns)
                 .filter(nv -> nv.getTruongPhong() == null)
                 .toList();
 
-        //lấy dsTruongPhong
         List<TruongPhong> dsTruongPhong = getNhanSu().stream()
                 .filter(ns -> ns instanceof TruongPhong)
                 .map(ns -> (TruongPhong) ns)
@@ -149,7 +144,6 @@ public class CongTy {
 
         TruongPhong tpChon = dsTruongPhong.get(tpIndex);
 
-        // Gán 2 bên TP NV
         nvChon.setTruongPhong(tpChon);
         tpChon.getNhanVien().add(nvChon);
 
@@ -170,6 +164,194 @@ public class CongTy {
         }
     }
 
+    public void tinhVaXuatTongLuong(){
+        System.out.println("\n======= TÍNH LƯƠNG TOÀN CÔNG TY =======");
+
+        double tongLuong = 0;
+        int stt = 1;
+
+        for (NhanSu ns : nhanSu) {
+            double luong = ns.tinhLuong();
+            System.out.printf("%d. [%s] %s - Lương: %.2f\n",
+                    stt++, ns.getMaSo(), ns.getHoTen(), luong);
+            tongLuong += luong;
+        }
+
+        System.out.printf("\nTổng lương toàn công ty: %.2f\n", tongLuong);
+        System.out.println("========================================");
+    }
+
+    public void timNhanVienLuongCaoNhat() {
+        System.out.println("\n======= NHÂN VIÊN CÓ LƯƠNG CAO NHẤT =======");
+
+        double maxLuong = -1;
+        List<NhanVien> topNhanVien = new ArrayList<>();
+
+        for (NhanSu ns : nhanSu) {
+            if(ns instanceof NhanVien nv) {
+                double luong = ns.tinhLuong();
+                if (luong > maxLuong) {
+                    maxLuong = luong;
+                    topNhanVien.clear();
+                    topNhanVien.add(nv);
+                }else if(luong == maxLuong) {
+                    topNhanVien.add(nv);
+                }
+            }
+        }
+
+        if(topNhanVien.isEmpty()){
+            System.out.println("Không có nhân viên nào trong công ty.");
+        }
+
+        System.out.printf("Lương cao nhất: %.2f\n", maxLuong);
+
+        for (int i = 0; i< topNhanVien.size(); i++){
+            NhanVien nv = topNhanVien.get(i);
+            System.out.printf("%d. [%s] %s - SĐT: %s - Ngày làm: %d\n",
+                    i + 1, nv.getMaSo(), nv.getHoTen(), nv.getSoDienThoai(), nv.getSoNgayLamViec());
+        }
+
+        System.out.println("============================================");
+    }
+
+    public void timTruongPhongQuanLyNhieuNhanVienNhat(){
+        System.out.println("\n======= TRƯỞNG PHÒNG QUẢN LÝ NHIỀU NHÂN VIÊN NHẤT =======");
+
+        int maxSoLuong = -1;
+        List<TruongPhong> topTP = new ArrayList<>();
+
+        for (NhanSu ns : nhanSu) {
+            if(ns instanceof TruongPhong tp) {
+                int soLuong = tp.getNhanVien().size();
+                if (soLuong > maxSoLuong) {
+                    maxSoLuong = soLuong;
+                    topTP.clear();
+                    topTP.add(tp);
+                } else if (soLuong == maxSoLuong) {
+                    topTP.add(tp);
+                }
+            }
+        }
+
+        if(topTP.isEmpty()){
+            System.out.println("Không có trưởng phòng nào trong công ty.");
+        }
+
+        System.out.printf("Số lượng nhân viên dưới quyền nhiều nhất: %d\n", maxSoLuong);
+        for(int i = 0; i < topTP.size(); i++){
+            TruongPhong tp = topTP.get(i);
+            System.out.printf("%d. [%s] %s - SĐT: %s - Ngày làm: %d - Số lượng NV: %d\n",
+                    i + 1, tp.getMaSo(), tp.getHoTen(), tp.getSoDienThoai(), tp.getSoNgayLamViec(), tp.getNhanVien().size());
+
+        }
+
+        System.out.println("==========================================================");
+    }
+
+    public void sapXepNhanVienTheoABC(){
+        System.out.println("\n======= DANH SÁCH NHÂN SỰ THEO THỨ TỰ TÊN =======");
+
+        List<NhanSu> sortNhanSu = new ArrayList<>(nhanSu);
+
+        sortNhanSu.sort(Comparator.comparing(ns -> ns.getHoTen().toLowerCase()));
+
+        int stt = 1;
+
+        for (NhanSu ns : sortNhanSu) {
+            String loai = ns instanceof NhanVien ? "Nhân Viên"
+                    : ns instanceof TruongPhong ? "Trưởng Phòng"
+                    : "Giám Đốc";
+
+            System.out.printf("%d. [%s] %s - (%s)\n", stt++, ns.getMaSo(), ns.getHoTen(), loai);
+        }
+
+        System.out.println("====================================================");
+    }
+
+    public void sapXepNhanSuTheoLuongGiamDan(){
+        System.out.println("\n======= DANH SÁCH NHÂN SỰ THEO LƯƠNG GIẢM DẦN =======");
+
+        List<NhanSu> sortNhanSu = new ArrayList<>(nhanSu);
+
+        sortNhanSu.sort((a,b) -> Double.compare(b.tinhLuong(), a.tinhLuong()));
+
+        int stt = 1;
+        for (NhanSu ns : sortNhanSu) {
+            String loai = ns instanceof NhanVien ? "Nhân Viên"
+                    : ns instanceof TruongPhong ? "Trưởng Phòng"
+                    : "Giám Đốc";
+
+
+            System.out.printf("%d. [%s] %s - Lương: %.2f - (%s)\n",
+                    stt++, ns.getMaSo(), ns.getHoTen(), ns.tinhLuong(), loai);
+        }
+
+        System.out.println("======================================================");
+
+    }
+
+    public void timGiamDocCoCoPhanNhieuNhat(){
+        System.out.println("\n======= GIÁM ĐỐC CÓ CỔ PHẦN CAO NHẤT =======");
+
+        List<GiamDoc> giamDoc = nhanSu.stream()
+                .filter(ns -> ns instanceof GiamDoc)
+                .map(ns -> (GiamDoc) ns)
+                .toList();
+
+        if(giamDoc.isEmpty()){
+            System.out.println("Không có giám đốc nào trong công ty.");
+        }
+
+        double maxCoPhan = giamDoc.stream()
+                .mapToDouble(GiamDoc::getCoPhan)
+                .max()
+                .orElse(0);
+
+        List<GiamDoc> topGD = giamDoc.stream()
+                .filter(gd -> gd.getCoPhan() == maxCoPhan)
+                .toList();
+
+
+        System.out.printf("Mức cổ phần cao nhất: %.2f%%\n", maxCoPhan);
+
+        int stt = 1;
+        for (GiamDoc gd : topGD) {
+            System.out.printf("%d. [%s] %s - SĐT: %s - Ngày làm: %d - Cổ phần: %.2f%%\n",
+                    stt++, gd.getMaSo(), gd.getHoTen(), gd.getSoDienThoai(),
+                    gd.getSoNgayLamViec(), gd.getCoPhan());
+        }
+
+        System.out.println("=============================================");
+    }
+
+    public void tinhVaXuatThuNhapCuaGiamDoc(){
+        System.out.println("\n======= THU NHẬP CỦA CÁC GIÁM ĐỐC =======");
+
+        List<GiamDoc> giamDoc = getNhanSu().stream()
+                .filter(ns -> ns instanceof GiamDoc)
+                .map(ns  -> (GiamDoc) ns)
+                .toList();
+
+        double tongLuongCongTy = 0;
+
+        double tongLuong = nhanSu.stream().mapToDouble(NhanSu::tinhLuong).sum();
+        double loiNhuanCongTy = doanhThuThang - tongLuong;
+
+        if(giamDoc.isEmpty()){
+            System.out.println("Không có giám đốc nào trong công ty.");
+        }
+
+        int stt = 1;
+        for (GiamDoc gd : giamDoc) {
+            double luong = gd.tinhLuong();
+            double thuNhap = luong + gd.getCoPhan() * loiNhuanCongTy;
+
+            System.out.printf("%d. Lợi Nhuân Công Ty: [%.2f] [%s] %s - Lương: %.2f - Cổ phần: %.2f%% → Tổng thu nhập: %.2f\n",
+                    stt++, loiNhuanCongTy, gd.getMaSo(), gd.getHoTen(), luong, gd.getCoPhan(), thuNhap);
+        }
+    }
+
     private void themNhanSuMoi(Scanner sc) {
         System.out.println("\n--- THÊM NHÂN SỰ ---");
         System.out.println("1. Nhân viên thường");
@@ -181,7 +363,6 @@ public class CongTy {
         System.out.print("Mã số: ");
         String maSo = sc.nextLine().trim();
 
-        // tìm mã trùng
         if (nhanSu.stream().anyMatch(ns -> ns.getMaSo().equalsIgnoreCase(maSo))) {
             System.out.println("Mã số đã tồn tại!");
             return;
@@ -202,6 +383,16 @@ public class CongTy {
             case 3 -> {
                 System.out.print("Cổ phần (%): ");
                 double coPhan = Double.parseDouble(sc.nextLine());
+
+                double coPhanHienTai = nhanSu.stream()
+                        .filter(ns -> ns instanceof GiamDoc)
+                        .mapToDouble(ns -> ((GiamDoc) ns).getCoPhan())
+                        .sum();
+
+                if(coPhanHienTai + coPhan > 100){
+                    System.out.println("Cổ phần hiện tại là %.2f%%. Nếu thêm sẽ vượt quá 100% cố phần!!!");
+                }
+
                 nsMoi = new GiamDoc(maSo, hoTen, sdt, soNgay, coPhan);
                 ((GiamDoc) nsMoi).setCongTy(this);
             }
@@ -289,7 +480,4 @@ public class CongTy {
 
         System.out.println("========================================================");
     }
-
-
-
 }
--- src/BaiTap11/Main.java
+++ src/BaiTap11/Main.java
@@ -1,78 +1,60 @@
 package BaiTap11;
 
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         CongTy congTy = new CongTy();
-        int choice;
+        int key;
 
-        do {
-            System.out.println("\n====== MENU QUẢN LÝ NHÂN SỰ ======");
-            System.out.println("1. Nhập thông tin công ty");
-            System.out.println("2. Phân bổ Nhân viên vào Trưởng phòng");
-            System.out.println("3. Thêm, xóa thông tin một nhân sự");
-            System.out.println("4. Xuất thông tin toàn bộ người trong công ty");
-            System.out.println("5. Tính và xuất tổng lương cho toàn công ty");
-            System.out.println("6. Tìm Nhân viên thường có lương cao nhất");
-            System.out.println("7. Tìm Trưởng phòng có số lượng nhân viên dưới quyền nhiều nhất");
-            System.out.println("8. Sắp xếp nhân viên toàn công ty theo thứ tự abc");
-            System.out.println("9. Sắp xếp nhân viên toàn công ty theo thứ tự lương giảm dần");
-            System.out.println("10. Tìm Giám đốc có số lượng cổ phần nhiều nhất");
-            System.out.println("11. Tính và Xuất tổng THU NHẬP của từng Giám đốc");
-            System.out.println("12. Tạo dữ liệu giả");
-            System.out.println("13. Thoát chương trình");
-            System.out.print("→ Nhập lựa chọn của bạn: ");
+        while (true) {
+            System.out.println("\n╔═══════════════════════════════════════════════╗");
+            System.out.println("║             MENU QUẢN LÝ NHÂN SỰ              ║");
+            System.out.println("╠═══════════════════════════════════════════════╣");
+            System.out.println("║ 1. Nhập thông tin công ty                     ║");
+            System.out.println("║ 2. Phân bổ Nhân viên vào Trưởng phòng         ║");
+            System.out.println("║ 3. Thêm, xóa thông tin một nhân sự            ║");
+            System.out.println("║ 4. Xuất thông tin toàn bộ người trong công ty ║");
+            System.out.println("║ 5. Tính và xuất tổng lương toàn công ty       ║");
+            System.out.println("║ 6. Tìm Nhân viên có lương cao nhất            ║");
+            System.out.println("║ 7. Trưởng phòng có nhiều nhân viên nhất       ║");
+            System.out.println("║ 8. Sắp xếp theo tên (A → Z)                   ║");
+            System.out.println("║ 9. Sắp xếp theo lương giảm dần                ║");
+            System.out.println("║ 10. Giám đốc có cổ phần nhiều nhất            ║");
+            System.out.println("║ 11. Tổng thu nhập từng Giám đốc               ║");
+            System.out.println("║ 12. Tạo dữ liệu giả mẫu                       ║");
+            System.out.println("║ 13. Thoát chương trình                        ║");
+            System.out.println("╚═══════════════════════════════════════════════╝");
 
-            choice = sc.nextInt();
-            sc.nextLine();
+            try {
+                System.out.print("-> Nhập lựa chọn của bạn (1–13): ");
+                key = Integer.parseInt(sc.nextLine());
 
-            switch (choice) {
-                case 1:
-                    congTy.nhapThongTinCongTy(sc);
-                    break;
-                case 2:
-                    congTy.phanBoNhanVienChoTruongPhong(sc);
-                    break;
-                case 3:
-                    congTy.themHoacXoaNhanSu(sc);
-                    break;
-                case 4:
-                    congTy.xuatThongTinToanBoNhanSu();
-                    break;
-                case 5:
-//                    congTy.tinhVaXuatTongLuong();
-                    break;
-                case 6:
-//                    congTy.timNhanVienThuongLuongCaoNhat();
-                    break;
-                case 7:
-//                    congTy.timTruongPhongQuanLyNhieuNhanVienNhat();
-                    break;
-                case 8:
-//                    congTy.sapXepNhanVienTheoTen();
-                    break;
-                case 9:
-//                    congTy.sapXepNhanVienTheoLuongGiamDan();
-                    break;
-                case 10:
-//                    congTy.timGiamDocCoCoPhanNhieuNhat();
-                    break;
-                case 11:
-//                    congTy.tinhVaXuatThuNhapGiamDoc();
-                    break;
-                case 12:
-                    congTy.taoDuLieuGia();
-                    break;
-                case 13:
-                    System.out.println("Đã thoát chương trình.");
-                default:
-                    System.out.println("Lựa chọn không hợp lệ. Vui lòng thử lại.");
+                switch (key) {
+                    case 1 -> congTy.nhapThongTinCongTy(sc);
+                    case 2 -> congTy.phanBoNhanVienChoTruongPhong(sc);
+                    case 3 -> congTy.themHoacXoaNhanSu(sc);
+                    case 4 -> congTy.xuatThongTinToanBoNhanSu();
+                    case 5 -> congTy.tinhVaXuatTongLuong();
+                    case 6 -> congTy.timNhanVienLuongCaoNhat();
+                    case 7 -> congTy.timTruongPhongQuanLyNhieuNhanVienNhat();
+                    case 8 -> congTy.sapXepNhanVienTheoABC();
+                    case 9 -> congTy.sapXepNhanSuTheoLuongGiamDan();
+                    case 10 -> congTy.timGiamDocCoCoPhanNhieuNhat();
+                    case 11 -> congTy.tinhVaXuatThuNhapCuaGiamDoc();
+                    case 12 -> congTy.taoDuLieuGia();
+                    case 13 -> {
+                        System.out.println("Đã thoát chương trình!!!");
+                        return;
+                    }
+                    default -> System.out.println("Lựa chọn không hợp lệ. Vui lòng nhập từ 1–13.");
+                }
+
+            } catch (NumberFormatException e) {
+                System.out.println("Vui lòng nhập một số nguyên từ 1 đến 13.");
             }
-
-        } while (choice != 13);
-
-        sc.close();
+        }
     }
 }
